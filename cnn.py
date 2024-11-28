85ec0d5662330b7e5c783df03161fd7c574c6115
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2024-11-28 17:41:03 +0900

    Add CNN and stft

---
+++ cnn.py
@@ -0,0 +1,97 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Conv1D, MaxPooling1D, Masking, Flatten, Dense
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+
+class CNN(): 
+    def __init__(self, scale = 661, time_range = 65, num_class = 3, start_filter = 8):
+        self.num_class = num_class
+        self.scale = scale
+        self.time_range = time_range                
+        self.model = tf.keras.models.Sequential([
+            Masking(mask_value=0.0, input_shape=(time_range, scale)),            
+            Conv1D(start_filter, 3, activation='relu'),  # 第1畳み込み層
+            MaxPooling1D(2),  # 第1プーリング層
+            Conv1D(start_filter * 2, 3, activation='relu'),  # 第2畳み込み層
+            MaxPooling1D(2),  # 第2プーリング層
+            Conv1D(start_filter * 2 * 2, 3, activation='relu'),  # 第3畳み込み層
+            MaxPooling1D(3),  # 第3プーリング層
+            Conv1D(start_filter * 2 * 2 * 2, 3, activation='relu'),  # 第4畳み込み層
+            MaxPooling1D(1),  # 第4プーリング層
+            Flatten(),  # データのフラット化
+            Dense(128, activation='relu'),
+            Dense(64, activation='relu'),
+            Dense(1, activation='sigmoid')
+        ])
+
+        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    def training(self, train_data, train_labels, epochs, batch_size, early_stopping = None, model_checkpoint = None):
+        if early_stopping == None and model_checkpoint == None:
+            self.model.fit(train_data, train_labels, epochs=epochs, validation_split=0.1, batch_size= batch_size)
+        elif early_stopping == None:
+            self.model.fit(train_data, train_labels, epochs=epochs, validation_split=0.1, batch_size= batch_size, callbacks=[model_checkpoint])
+        elif model_checkpoint == None:
+            self.model.fit(train_data, train_labels, epochs=epochs, validation_split=0.1, batch_size= batch_size, callbacks=[early_stopping])
+        else:
+            self.model.fit(train_data, train_labels, epochs=epochs, validation_split=0.1, batch_size= batch_size, callbacks=[early_stopping, model_checkpoint])
+        
+    def evaluate(self, test_data, test_labels):
+        self.test_loss, self.test_accuracy = self.model.evaluate(test_data, test_labels)
+        print("Test accuracy: ", self.test_accuracy)
+        self.predictions = self.model.predict(test_data)
+        self.predicted_classes = np.argmax(self.predictions, axis=1)
+        self.true_classes = np.argmax(test_labels, axis=1)
+        self.correctly_classified = self.predicted_classes == self.true_classes        
+        self.correct_indices = np.where(self.correctly_classified)[0]
+        self.incorrect_indices = np.where(~self.correctly_classified)[0]
+
+        print("正しく分類されたサンプルのインデックス:", self.correct_indices)
+        print("誤って分類されたサンプルのインデックス:", self.incorrect_indices)
+        for i in self.incorrect_indices:
+            print(f"サンプル {i}: 正解 = {self.true_classes[i]}, 予測 = {self.predicted_classes[i]}")
+
+    def evaluate_print(self):
+        print(self.predictions)
+        print(self.predicted_classes)
+        print(self.true_classes)
+        print(self.correctly_classified)
+        print(self.correct_indices)
+        print(self.incorrect_indices)
+
+    def save(self, file_name):
+        self.model.save(file_name)
+
+if __name__ == "__main__":
+    from .dataset_stft import DataSetSTFT
+    import pathlib
+    import numpy as np
+    directory_path = pathlib.Path('C:\\Users\\S2\\Documents\\デバイス作成\\2024測定デバイス\\swallowing\\dataset')
+   
+    train_voice_folder = directory_path / 'washino' / 'voice'
+    train_cough_folder = directory_path / 'washino' / 'cough'
+    train_swallowing_folder = directory_path / 'washino' / 'swallowing'    
+
+    test_voice_folder = directory_path / 'shibata' / 'voice'
+    test_cough_folder = directory_path / 'shibata' / 'cough'
+    test_swallowing_folder = directory_path / 'shibata' / 'swallowing'    
+    
+    # train_data = VariableDataSet()
+    test_data = DataSetSTFT(num_samples=28)
+
+    # train_data.folder_to_dataset(train_swallowing_folder, np.array(0))
+    # train_data.folder_to_dataset(train_cough_folder, np.array(1))    
+    # train_data.folder_to_dataset(train_voice_folder, np.array([1, 0, 0]), 2)
+    # train_data.print_label()
+    test_data.folder_to_dataset(test_swallowing_folder, np.array(0), 0)
+    test_data.folder_to_dataset(test_cough_folder, np.array(1), 14)
+
+    # test_data.folder_to_dataset(test_voice_folder, np.array([1, 0, 0]), 2)
+
+    model = CNN()
+    # model.training(train_data.data, train_data.labels, 1, 32)
+    model.training(test_data.data, test_data.labels, 2, 32)
+    # model.evaluate(test_data.data, test_data.labels)
+    # model.save('20240116_159datasets.keras')
---
+++ dataset_stft.py
@@ -0,0 +1,92 @@
+import numpy as np
+import pathlib
+import os
+
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler
+
+from .audio import Audio
+from .stft import STFT
+
+
+class DataSetSTFT():
+    def __init__(self, num_samples, scale = 661, time_range = 65):
+        self.time_range = time_range        
+        self.scale = scale
+        self.data = np.zeros((num_samples, self.time_range, self.scale))
+        self.labels = np.zeros(num_samples)
+        self.max_cols = 0
+
+    def add_to_dataset(self, i, data, label):        
+        if type(data) == tuple:
+            spectrogram = np.abs(data)        
+        else:
+            spectrogram = data         
+            
+        if len(spectrogram) == 0:
+            print("Warning: No data available for FFT.") 
+            print(i)
+            print(spectrogram)
+            return 
+            
+        scaler_X = MinMaxScaler()        
+        if spectrogram.ndim == 1:
+            spectrogram = spectrogram.reshape(-1, 1)  # 1次元配列を2次元配列に変換               
+        normalized_spectrogram = scaler_X.fit_transform(spectrogram)
+                                 
+        data = self.trim_or_pad(normalized_spectrogram)  
+        data = data.reshape(self.time_range, self.scale)       
+            
+        self.data[i] = data
+        self.labels[i] = label
+
+    def trim_or_pad(self, data):
+        current_length = data.shape[1]        
+        if current_length > self.time_range:
+            # time_range以上の場合はトリミング            
+            trimmed_data = data[:, :self.time_range]       
+            return trimmed_data
+        elif current_length < self.time_range:
+            # time_range未満の場合はパディング
+            padding_length = self.time_range - current_length
+            padded_data = np.pad(data, ((0, 0), (0, padding_length)), mode='constant', constant_values=0)
+            return padded_data
+        else:
+            # すでにtime_rangeの場合はそのまま返す
+            return data  
+        
+    def folder_to_dataset(self, folder_name, label, start_num):        
+        file_names = self.get_wav_files(folder_name)
+        for i, file_name in enumerate(file_names):
+            wav = Audio(folder_name / file_name)
+            wavdata = STFT(wav.sample_rate, wav.trimmed_data, )
+            spectrogram = wavdata.generate_spectrogram()
+            self.add_to_dataset(start_num + i, spectrogram, label)
+
+    def get_wav_files(self, directory):
+        wav_files = []
+        for filename in os.listdir(directory):
+            if filename.endswith(".wav"):
+                wav_files.append(filename)
+        return wav_files
+
+if __name__ == "__main__":    
+    path = pathlib.Path('C:/Users/S2/Documents/デバイス作成/2024測定デバイス/swallowing/dataset/washino/voice/voice12.wav')
+    wav1 = Audio(path)
+    swallowing1 = STFT(wav1.sample_rate, wav1.trimmed_data, )
+    spectrogram =  swallowing1.generate_spectrogram()
+    print(spectrogram.shape)
+    data = DataSetSTFT(15)
+    label = np.array(0)
+    data.add_to_dataset(0, spectrogram, label)
+    print(len(data.data))
+    print(len(data.data[0]))
+    print(len(data.data[0][2]))
+    print(data.labels)
+
+    directory_path = pathlib.Path('C:\\Users\\S2\\Documents\\デバイス作成\\2024測定デバイス\\swallowing\\dataset')   
+    train_voice_folder = directory_path / 'shibata' / 'voice'
+    data.folder_to_dataset(train_voice_folder, label, 1)
+    print(len(data.data))
+    print(len(data.data[0]))
+    print(len(data.data[0][5]))
